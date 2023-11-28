a27d956dbf0b7f135b6cf5a43e5cc8585837bcbd
m-ox <maudlin.oxalis@gmail.com>
2023-11-28 12:39:06 -0700

    stringy business

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        String text = "good morning starshine";
+        String text2 = " the \"world\" says hello";
+        System.out.println(text.toUpperCase());
+        System.out.println(text.indexOf("o"));
+        System.out.println(text + text2);
+    }
+}
