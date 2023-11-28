0562ac057c5ac9f53b3541119d648df2ca7e453b
m-ox <maudlin.oxalis@gmail.com>
2023-11-28 12:15:04 -0700

    data types, numbe

--- Variables.java
+++ Variables.java
@@ -1,6 +1,6 @@
 public class Variables {
     public static void main(String[] args) {
-        double myNumber = 123;
+        int myNumber = 123;
         String myString = "kayak";
         System.out.println(myNumber + " " + myString);
       }  
