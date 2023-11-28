dc33daebb006eb0386cb1ae1de2ed013c532a04b
m-ox <maudlin.oxalis@gmail.com>
2023-11-28 12:20:26 -0700

    operators

---
+++ Operators.java
@@ -0,0 +1,6 @@
+public class Operators {
+    public static void main(String[] args) {
+        int x = 100 + 60 - (500 / 50 * 2);
+        System.out.println(x);
+    }
+}
--- Variables.java
+++ Variables.java
@@ -1,5 +1,10 @@
 public class Variables {
     public static void main(String[] args) {
+        // Widening Casting (automatically) - converting a smaller type to a larger type size
+        // byte -> short -> char -> int -> long -> float -> double
+
+        // Narrowing Casting (manually) - converting a larger type to a smaller size type
+        // double -> float -> long -> int -> char -> short -> byte
         int myNumber = 123;
         double myDouble = myNumber;
         System.out.println(myDouble);
