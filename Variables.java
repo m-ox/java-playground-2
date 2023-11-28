33942a242ba6a92ba5e85b441dffa991700d9f1c
m-ox <maudlin.oxalis@gmail.com>
2023-11-28 12:14:17 -0700

    variables, double and string

---
+++ Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    public static void main(String[] args) {
+        double myNumber = 123;
+        String myString = "kayak";
+        System.out.println(myNumber + " " + myString);
+      }  
+}
