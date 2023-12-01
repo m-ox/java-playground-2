075a76d6602f01d64dc0526c36209cbe6c85a212
m-ox <maudlin.oxalis@gmail.com>
2023-11-30 23:30:14 -0700

    hmm

--- Challenges/MinutesToYearsDaysCalculator.java
+++ Challenges/MinutesToYearsDaysCalculator.java
@@ -2,15 +2,15 @@ package Challenges;
 
 public class MinutesToYearsDaysCalculator {
     public static void printYearsAndDays(long minutes) {
-        if (minutes < 0) {
+        if (minutes > 0) {
+            long hours = minutes / 60L;
+            long days = hours / 24L;
+            long years = days / 365L;
+            long daysLeft = days % 365L;
+            
+            System.out.println(minutes + " min = " + years + " y and " + daysLeft + " d");
+        } else {
             System.out.println("Invalid Value");
-            return;
         }
-        long hours = minutes / 60L;
-        long days = hours / 24L;
-        long years = days / 365L;
-        long daysLeft = days % 365L;
-        
-        System.out.println(minutes + " min = " + years + " y and " + daysLeft + " d");
     }
 }
