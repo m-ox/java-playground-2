1368a7c13d07067e12ab2ef84690c7f13aa58e5e
m-ox <maudlin.oxalis@gmail.com>
2023-11-30 23:11:39 -0700

    easy equal sum checker

---
+++ Challenges/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package Challenges;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        // this is a bit easy out of nowhere
+        return num1 + num2 == num3;
+    }
+}
