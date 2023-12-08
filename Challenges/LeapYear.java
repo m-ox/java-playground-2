002edac9ed694fb439d3ea25ad79a9df2515cdf6
m-ox <maudlin.oxalis@gmail.com>
2023-12-08 11:13:32 -0500

    get days of month

--- Challenges/LeapYear.java
+++ Challenges/LeapYear.java
@@ -20,4 +20,17 @@ public class LeapYear {
             return false;
         }
     }
+    
+    public static int getDaysInMonth (int month, int year) {
+        if ((month < 1 || month > 12) || (year < 1 || year > 9999)) {
+            return -1;
+        } else {
+            return switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12 -> 31;
+                case 4, 6, 9, 11 -> 30;
+                case 2 -> isLeapYear(year) ? 29: 28;
+                default -> -1;
+            };
+        }
+    }
 }
